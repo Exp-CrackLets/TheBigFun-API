bf6a186d4830516095e177d8c1155538fbae7781
RauVR <u201714049@upc.edu.pe>
2023-09-26 16:16:13 -0500

    feat: AttendeeService Unit Test Implemented

---
+++ src/test/java/com/crackelets/bigfun/platform/profile/service/AttendeeServiceTest.java
@@ -0,0 +1,78 @@
+package com.crackelets.bigfun.platform.profile.service;
+
+import com.crackelets.bigfun.platform.payment.domain.model.Payment;
+import com.crackelets.bigfun.platform.payment.domain.persistence.PaymentRepository;
+import com.crackelets.bigfun.platform.profile.domain.model.Attendee;
+import com.crackelets.bigfun.platform.profile.domain.persistence.AttendeeRepository;
+import com.crackelets.bigfun.platform.shared.exception.ResourceNotFoundException;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+
+import javax.validation.Validator;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+@RunWith(MockitoJUnitRunner.class)
+public class AttendeeServiceTest {
+
+    @InjectMocks
+    private AttendeeServiceImpl attendeeService;
+
+    @Mock
+    private AttendeeRepository attendeeRepository;
+
+    @Mock
+    private Validator attendeeValidator;
+
+    @Test
+    @DisplayName("Get All Attendees- Ok")
+    public void testGetAllAttendees() {
+        List<Attendee> expected = Arrays.asList(new Attendee(), new Attendee());
+        when(attendeeRepository.findAll()).thenReturn(expected);
+        List<Attendee> actual = attendeeService.getAll();
+        Assertions.assertEquals(expected.size(), actual.size());
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
