3303339cb282452f039c41e06c417e0b881c78e7
Diego Santivañez <Diegojsf17@gmail.com>
2023-11-20 22:42:04 -0500

    feat: updated

--- src/test/java/com/crackelets/bigfun/platform/BigFunApplicationTests.java
+++ src/test/java/com/crackelets/bigfun/platform/BigFunApplicationTests.java
@@ -2,10 +2,24 @@ package com.crackelets.bigfun.platform;
 
 import com.crackelets.bigfun.platform.payment.service.PaymentServiceTest;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 
-@SpringBootTest
 class BigFunApplicationTests {
 
+    @Test
+    void testApplication() {
+        MockedStatic<SpringApplication> utilities = Mockito.mockStatic(SpringApplication.class);
+        utilities.when((MockedStatic.Verification) SpringApplication.run(BigFunApplication.class, new String[]{})).thenReturn(null);
+        BigFunApplication.main(new String[]{});
+        assertThat(SpringApplication.run(BigFunApplication.class, new String[]{})).isEqualTo(null);
+    }
 }
