d4445e29e26e6cad4ebc9013e23fb41fc1ac2fb8
Diego Santivañez <Diegojsf17@gmail.com>
2023-11-20 22:23:59 -0500

    feat: pom updated

--- src/test/java/com/crackelets/bigfun/platform/BigFunApplicationTests.java
+++ src/test/java/com/crackelets/bigfun/platform/BigFunApplicationTests.java
@@ -1,13 +1,15 @@
 package com.crackelets.bigfun.platform;
 
+import com.crackelets.bigfun.platform.payment.service.PaymentServiceTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class BigFunApplicationTests {
 
     @Test
     void contextLoads() {
+        PaymentServiceTest paymentServiceTest = new PaymentServiceTest();
     }
-
 }
